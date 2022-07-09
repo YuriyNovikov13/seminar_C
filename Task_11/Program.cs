23342cfbc3b90728e64295af8c5a5c7e05e1046f
Yuriy Novikov <7413856@mail.ru>
2022-07-10 01:21:09 +0300

    решение через метод

--- Task_11/Program.cs
+++ Task_11/Program.cs
@@ -7,14 +7,26 @@
 int rndNum = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное трехзначное число {rndNum}");
 int firstDigit = rndNum / 100;
-int secondDigit = rndNum / 10;
-int thirdDigit = rndNum % 10;
-// // Console.WriteLine($"число с удаленной второй цифрой {firstDigit}{thirdDigit}");
-int num = (firstDigit*10+thirdDigit);
+int secondDigit = rndNum % 10;
+
+// // Console.WriteLine($"число с удаленной второй цифрой {firstDigit}{secondDigit}");
+int num = (firstDigit * 10 + secondDigit);
 Console.WriteLine($"число с удаленной второй цифрой {num}");
 
 // простое решение:
 int rndNum = new Random().Next(100, 1000);
 Console.WriteLine($"Случайное трехзначное число {rndNum}");
-int newNum = rndNum/100*10+rndNum%10;
+int newNum = rndNum / 100 * 10 + rndNum % 10;
 Console.WriteLine($"число с удаленной второй цифрой {newNum}");
+
+// решение через метод
+int rndNum = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число {rndNum}");
+int NewNum(int num)
+{
+    int firstDigit = rndNum / 100;
+    int secondDigit = rndNum % 10;
+    return firstDigit * 10 + secondDigit;
+}
+int result = NewNum(rndNum);
+Console.WriteLine($"число с удаленной второй цифрой {nresult}");
